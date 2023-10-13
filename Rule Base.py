978b7f05ff2efe9f9185d4e4ca1bd1072fc9ad6a
Justin Bolivar <justinantonio.bolivar.21@usjr.edu.ph>
2023-10-13 16:49:57 +0800

    added weapon banner calculatiions and moved button down

--- Rule Base.py
+++ Rule Base.py
@@ -3,35 +3,41 @@ import tkinter as tk
 CHARACTER_BANNER = "Character Banner"
 WEAPON_BANNER = "Weapon Banner"
 
-def calculate_probability(event=None):
+def calculate_probability():
     try:
         pulls = int(entry.get())
+        banner_type = banner_choice.get()
+
         if pulls <= 0:
             result_label.config(text="Please enter a valid number of pulls.")
-        elif pulls > 90:
-            result_label.config(text=f"Invalid pull amount! Pulls cannot exceed 90 (count pulls from 
-                                last obtained 5 star)")
+        elif banner_type == CHARACTER_BANNER:
+            probability = calculate_probability_for_banner(pulls, CHARACTER_BANNER)
+            result_label.config(text=f"Character Banner: Probability of a 5-star character: {probability:.2%}")
+        elif banner_type == WEAPON_BANNER:
+            probability = calculate_probability_for_banner(pulls, WEAPON_BANNER)
+            result_label.config(text=f"Weapon Banner: Probability of a 5-star weapon: {probability:.2%}")
         else:
-            probability = calculate(pulls)
-            result_label.config(text=f"Probability of Obtaining a 5-Star Character: {probability:.2%}")
+            result_label.config(text="Please select a banner type.")
+
     except ValueError:
         result_label.config(text="Invalid input. Please enter a valid number.")
 
-def calculate(pulls):
-    if pulls < 73:
-        return 0.006
-    elif pulls < 90:
-        return 0.006 + (pulls - 73) * 0.006 * 10
-    else:
-        return 1.0
-    
-def calculate_weapon_probability(pulls):
-    if pulls < 63:
-        return 0.007
-    elif pulls < 80:
-        return 0.007 + (pulls - 63) * 0.007 * 10
-    else:
-        return 1.0    
+def calculate_probability_for_banner(pulls, banner_type):
+    if banner_type == CHARACTER_BANNER:
+        if pulls < 73:
+            return 0.006
+        elif pulls < 90:
+            return 0.006 + (pulls - 73) * 0.006 * 10
+        else:
+            return 1.0
+        
+    elif banner_type == WEAPON_BANNER:
+        if pulls < 62:
+            return 0.007
+        elif pulls < 77:
+            return 0.007 + (pulls - 62) * 0.007 * 10  # Increasing by 0.093 over 14 pulls
+        else:
+            return 1.0
 
 window = tk.Tk()
 window.title("Genshin Impact 5 star Calculator")
@@ -42,8 +48,15 @@ instruction_label.pack(pady = 10)
 entry = tk.Entry(window)
 entry.pack(pady = 5)
 
+banner_choice = tk.StringVar()
+character_banner_checkbox = tk.Radiobutton(window, text=CHARACTER_BANNER, variable=banner_choice, value=CHARACTER_BANNER)
+character_banner_checkbox.pack()
+weapon_banner_checkbox = tk.Radiobutton(window, text=WEAPON_BANNER, variable=banner_choice, value=WEAPON_BANNER)
+weapon_banner_checkbox.pack()
+
 calculate_button = tk.Button(window, text="Calculate", command=calculate_probability)
 calculate_button.pack(pady = 10)
+
 
 result_label = tk.Label(window, text="")
 result_label.pack(pady = 10)
